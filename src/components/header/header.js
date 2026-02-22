5b31816b4d2dc1c7baf826f8d1666c7093dd66cf
Atte <atte.ikola@gmail.com>
2026-02-22 16:18:23 +0200

    Add animation to title and fixed color scheeme

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,37 @@
 import "./header.css";
+import { TypeAnimation } from 'react-type-animation';
+
+
+const namesAndTimes = [
+              'Software Developer',
+              2500,
+              'Data Scientist',
+              2500,
+              'Student',
+              2500,
+              'Web Developer',
+              2500,
+              'Bachlor of Science',
+              2500,
+            ]
+
+
 function Header() {
   return (
-    <div className="Header" >
-      <text>Header Content</text>
-    </div>
+    <header className="header">
+      <div>
+        <div>
+          <TypeAnimation
+            sequence={namesAndTimes}
+            wrapper="span"
+            speed={40}
+            style={{ display: 'inline-block' }}
+            repeat={Infinity}
+          />
+        </div>
+        <div className="name">Atte Ikola</div>
+      </div>
+    </header>
   );
 }
 
