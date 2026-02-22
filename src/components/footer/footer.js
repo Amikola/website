a1ffddcf69c718e77be8ab0339e70451b198a28b
Atte <atte.ikola@gmail.com>
2026-02-22 15:52:16 +0200

    Set up color pallet and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,26 @@
-import './footer.css';
+import "./footer.css";
+
 function Footer() {
   return (
-    <div className="Footer">
-      <text>Footer Content</text>
-    </div>
+    <footer className="footer">
+      <div>
+        <span>
+          <a href="https://github.com/Amikola" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+          {" | "}
+          <a href="https://www.linkedin.com/in/atte-ikola-35941a263" target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </a>
+          {" | "}
+          <a href="mailto:atte.ikola@gmail.com">
+            atte.ikola@gmail.com
+          </a>
+        </span>
+        <br />
+        <span>© 2026 Atte Ikola. All rights reserved.</span>
+      </div>
+    </footer>
   );
 }
 
